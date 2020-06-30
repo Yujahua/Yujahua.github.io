6fbfd854324bc0fde19f17f4c698f5abbf52aee5
yujiahua <yujiahua@csii.com.cn>
2020-06-30 20:47:46 +0800

    Replace homepage

---
+++ zorro/src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="welcome">
+        <header>
+            ...
+        </header>
+        <img alt="Zorro logo" width=100 height=100 src="../assets/8346747.png">
+        <h3>{{ msg }}</h3>
+        <footer>页脚</footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.welcome {
+    color: 
+}
+</style>
