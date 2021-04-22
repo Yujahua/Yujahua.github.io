8bbbf58cf327a7d8ff29566bd7e3c5b59379a705
yujahua <yujahua@163.com>
2021-04-22 23:12:29 +0800

    build(v0.9.0):production for master.io

---
+++ dist/precache-manifest.060d112a2e6fea08b0ee7c2141f887ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bc11e649ff1998c7bb0f0816d0b52f",
+    "url": "app.css"
+  },
+  {
+    "revision": "06832d753540e98ea2ac",
+    "url": "css/app.2a186d0d.css"
+  },
+  {
+    "revision": "aa57314a48e628f1030804d9586a37cc",
+    "url": "index.html"
+  },
+  {
+    "revision": "06832d753540e98ea2ac",
+    "url": "js/app.900f3964.js"
+  },
+  {
+    "revision": "e16a652d6ea199ce221e",
+    "url": "js/chunk-vendors.5b39a8e1.js"
+  },
+  {
+    "revision": "b8d72651f3b919646e595b82d36d7e53",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "f157fe16f635b0aff4b0955c6ff30f90",
+    "url": "reset.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
